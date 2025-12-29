e8735140f636a22aa9ddd9fde38aec4872d5b69f
nikolai <schimkeNikolai@gmail.com>
2025-12-29 21:58:35 +0000

    feat: Add clippy and rust-analyzer to flake, and ignore result directory.

--- flake.nix
+++ flake.nix
@@ -6,24 +6,54 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
         };
 
-        naersk' = pkgs.callPackage naersk {};
+        naersk' = pkgs.callPackage naersk { };
 
-      in rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
+      in
+      {
+        packages = {
+          # For `nix build` & `nix run`:
+          default = naersk'.buildPackage {
+            src = ./.;
+          };
+          # Run `nix build .#check` to check code
+          check = naersk'.buildPackage {
+            src = ./.;
+            mode = "check";
+          };
+          # Run `nix build .#test` to run tests
+          test = naersk'.buildPackage {
+            src = ./.;
+            mode = "test";
+          };
+          # Run `nix build .#clippy` to lint code
+          clippy = naersk'.buildPackage {
+            src = ./.;
+            mode = "clippy";
+          };
         };
 
         # For `nix develop` (optional, can be skipped):
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            rust-analyzer
+
+          ];
         };
       }
     );
